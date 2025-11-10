1a16e58fdeb714a03364818d0830814ef9b116b2
gogazub <gogazub101@gmail.com>
2025-11-10 22:57:33 +0300

    fear: add graceful shutdown

--- worker-service/cmd/main.go
+++ worker-service/cmd/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gogazub/consumer/config"
 	"github.com/gogazub/consumer/consumer"
@@ -26,21 +31,59 @@ func main() {
 	err = db.Ping()
 	must(err)
 
+	wg := sync.WaitGroup{}
+
 	orderRepo := repository.NewOrderRepository(db)
 	msgService := service.NewMessageService(*codeRunner, orderRepo)
 
 	taskConsumer := consumer.NewTaskConsumer(msgService)
 	taskConsumer.Connect(config.Cfg.RABBITMQ_URL)
-	taskConsumerCtx, taskConsumerCancel := context.WithCancel(context.Background())
+	taskConsumerCtx, cancel := context.WithCancel(context.Background())
+
+	errCh := make(chan error, 1)
+
+	wg.Add(1)
 	go func() {
-		taskConsumer.StartConsuming(taskConsumerCtx)
+		defer wg.Done()
+		if err := taskConsumer.StartConsuming(taskConsumerCtx); err != nil && !errors.Is(err, context.Canceled) {
+			errCh <- err
+		}
 	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-errCh:
+		log.Printf("component error: %v - initiating shutdown", err)
+		cancel()
+	case sig := <-sigCh:
+		log.Printf("signal %v received - initiating shutdown", sig)
+		cancel()
+	}
+
+	done := make(chan struct{})
+
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	timeout := 40 * time.Second
+	select {
+	case <-done:
+		log.Println("all components stopped gracefully")
+	case <-time.After(timeout):
+		log.Printf("timeout (%s) waiting for components to stop, exiting", timeout)
+	}
+
+	log.Println("shutdown complete")
 
 }
 
 func must(err error) {
 	if err != nil {
-		log.Printf(err.Error())
+		log.Print(err.Error())
 		os.Exit(1)
 	}
 }
